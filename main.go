2e7a956d72fa43fc46fcae03742707645341fa8a
DESKTOP-MJNVR7N\connapotae <connapotae@gmail.com>
2023-06-20 22:15:38 +0700

    -feature :sparkles: add mul func

--- main.go
+++ main.go
@@ -5,7 +5,14 @@ import "fmt"
 func sum(a, b int) int {
 	return a + b
 }
+
+func mul(a, b int) int {
+	return a * b
+
+}
+
 func main() {
 	fmt.Println("Hello Go")
 	fmt.Println(sum(10, 58))
+	fmt.Println(mul(10, 3))
 }
