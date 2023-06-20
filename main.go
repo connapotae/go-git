cd5eebd929ff61e08ade5d3c443eb302b4029984
DESKTOP-MJNVR7N\connapotae <connapotae@gmail.com>
2023-06-20 22:14:02 +0700

    -feature :spakrles: develop add sub

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+func sub(a, b int) int {
+	return a - b
+
+}
+
 func sum(a, b int) int {
 	return a + b
 }
 func main() {
 	fmt.Println("Hello Go")
 	fmt.Println(sum(10, 58))
+	fmt.Println(sub(10, 20))
 }
